66045819bfbf1e8e7d846ad9265fbf68ea6ecfb5
Connie Leung <cleung334@gmail.com>
2017-11-25 22:27:06 +0800

    Apply bootstrap 4 and font-awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ import {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'Angular + Redux Todo App';
