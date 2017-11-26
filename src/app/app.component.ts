450e62cf17d3f5d78ecbf9ef304ea1bea44fcf6a
Connie Leung <cleung334@gmail.com>
2017-11-26 17:58:36 +0800

    Confirm dialog for removeAll and delete todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import {
   TOGGLE_DONE,
   REMOVE_TODOS
 } from './reducers/todo.actions';
+import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'app-root',
@@ -21,10 +22,12 @@ import {
 export class AppComponent implements OnInit {
   title = 'Angular + Redux Todo App';
   todos$: Observable<TodoModel[]>;
-  todo: string = '';
+  newTodo: string = '';
   modTodo: string = '';
   editing: boolean = false;
   indexToEdit: number | null;
+
+
 
   constructor(private titleService: Title,
     private store: Store<AppStore>, private modalService: NgbModal) {
@@ -36,13 +39,13 @@ export class AppComponent implements OnInit {
   }
 
   addTodo() {
-    this.store.dispatch({ type: ADD_TODO, payload: { value: this.todo, done: false } });
-    this.todo = '';
+    this.store.dispatch({ type: ADD_TODO, payload: { value: this.newTodo, done: false } });
+    this.newTodo = '';
   }
 
   deleteTodo(index) {
-    this.store.dispatch({ type: DELETE_TODO, payload: { index } });    this.cancelEdit();
-
+    this.store.dispatch({ type: DELETE_TODO, payload: { index } });
+    this.cancelEdit();
   }
 
   editTodo(todo, index) {
@@ -76,13 +79,33 @@ export class AppComponent implements OnInit {
     return this.editing && index === this.indexToEdit;
   }
 
-  open(content, data) {
+  open(content) {
     this.modalService.open(content).result.then(result => {
        if (result === 'ConfirmRemoveAll') {
           this.removeTodos();
-       } else if (result === 'ConfirmDelete') {
-         this.deleteTodo(data.index);
        }
      }, () => {});
+  }
+
+  openAllRemove(content) {
+     const modalRef = this.modalService.open(ConfirmModalComponent);
+     modalRef.componentInstance.message = 'Are you sure to clear all todo items?';
+     modalRef.componentInstance.title = 'Remove all todo items';
+     modalRef.result.then(result => {
+       if (result === 'Confirm') {
+         this.removeTodos();
+       }
+     }, () => {});
+  }
+
+  openConfirmDelete(todo: TodoModel, index: number) {
+    const modalRef = this.modalService.open(ConfirmModalComponent);
+    modalRef.componentInstance.message = `Are you sure to delete "${todo.value}"?`;
+    modalRef.componentInstance.title = 'Delete todo item';
+    modalRef.result.then(result => {
+      if (result === 'Confirm') {
+        this.deleteTodo(index);
+      }
+    }, () => {});
   }
 }
